89277a4bf64b6a4cfc67388857a1dd7d46a4bde2
Manuel Calle <callemac88@gmail.com>
2022-11-05 18:37:11 -0500

    class 1

---
+++ script.js
@@ -0,0 +1,36 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.getElementsByClassName('parrafito');
+const pid = document.getElementById('pid');
+const input = document.querySelector('input');
+
+console.log(input.value)
+
+console.log({
+  h1,
+  p,
+  parrafito,
+  pid,
+  input,
+});
+
+h1.innerHTML= 'Patito <br /> Feo';
+
+// console.log(h1.getAttribute('class'));
+// h1.setAttribute('class', 'rojo');
+
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.conteains('verde');
+
+input.value = "456";
+
+const img = document.createElement('img');
+
+img.setAttribute('src', 'https://st2.depositphotos.com/1013220/6981/i/950/depositphotos_69812475-stock-photo-african-sunset-with-tree-in.jpg')
+
+console.log(img);
+
+pid.innerHTML = "";
+pid.appendChild(img);
